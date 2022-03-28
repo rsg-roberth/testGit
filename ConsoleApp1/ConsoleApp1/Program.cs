083f960289b82fa96d7063d9cd7483323f0b64e1
Roberth <rsg_roberth@yahoo.com.br>
2022-03-28 13:52:06 -0300

    Adicionando nome

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,6 +7,9 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var test = "Roberth Santos Gomes";
+            Console.WriteLine(test);
         }
     }
 }
